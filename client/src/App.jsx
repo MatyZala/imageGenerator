ea981bd66e397f7934aca61625a92d9d606082f4
MatyZalazar <mzx631@gmail.com>
2023-01-27 19:33:10 -0300

    update styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className='w-full flex
-    justify-between items-center bg-white
+    justify-between items-center bg-[#b8b8fc]
     sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to='/'>
           <img src={logo} alt='logo'
@@ -18,9 +18,9 @@ const App = () => {
         <Link to='/create-post'
           className='font-inter font-medium
       bg-[#6469ff] text-white px-4 py-2
-      rounded-md'>Create</Link>
+      rounded-md'>Create Image</Link>
       </header>
-      <main className='sm: p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+      <main className='sm: p-8 px-4 py-8 w-full bg-[#e1e1e2] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/create-post' element={<CreatePost />} />
